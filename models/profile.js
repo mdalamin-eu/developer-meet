fd5960345bce0ed76b3561d218315e6a95fa2554
Md Alamin <mdalamin.eu@gmail.com>
2020-10-12 20:10:04 +0300

    experienceAddtoProfile

--- models/profile.js
+++ models/profile.js
@@ -1,3 +1,7 @@
+
+//Hey Mr. Miraz I have some questions . So please go down slowly and give my questions answer ): 
+
+
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 const ProfileSchema = new mongoose. Schema({
@@ -30,5 +34,34 @@ bio:{
 githubusername:{
     type:String
 },
+experience: [
+    {
+        title: {
+            type: String,
+            required: true       //if i don't add exprince on my profile. it will be create any problem? bcoz It's have required:true !!
+    },
+        company:{
+            type: String,
+            required: true
+    },
+        location: {
+            type: String
+    },
+        from: {
+            type: Date,
+            required: true
+        },
+        to: {
+            type: Date
+        },
 
+        current: {
+            type: Boolean,
+            default: false   //why default and false?
+        },
+        description: {
+            type: String
+        }
+    }
+],
 })
