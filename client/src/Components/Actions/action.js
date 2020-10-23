c8e883beb2cdbdca1266a709697375226b038844
Md Alamin <mdalamin.eu@gmail.com>
2020-10-24 02:02:28 +0300

    IndexJS

--- client/src/Components/Actions/action.js
+++ client/src/Components/Actions/action.js
@@ -1,5 +1,5 @@
 import {REGISTER_USER } from './Types'
-export const registerAction= (data) => async dispatch =>
+export const regAction= (data) => async dispatch =>
 {
     dispatch({type:REGISTER_USER, payload:data});
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+import rootReducer from './Components/rootReducers/index'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import reduxThunk from 'redux-thunk'
+import App from './Components/App';
 
-import App from './App';
+const store = createStore(
+  rootReducer, composeWithDevTools(applyMiddleware(reduxThunk))
+)
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
