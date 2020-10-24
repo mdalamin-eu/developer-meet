18e4c36f30ec6de79ad6bb23755db10430ffe39d
Md Alamin <mdalamin.eu@gmail.com>
2020-10-24 18:55:19 +0300

    AddPublicHTMLindexCSS

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import {createStore, applyMiddleware} from 'redux'
 import rootReducer from './Components/rootReducers/index'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import reduxThunk from 'redux-thunk'
+import './index.css';
 import App from './App';
 
 const store = createStore(
