146616dc99c6528066fb8f5a16e991b044999191
Md Alamin <mdalamin.eu@gmail.com>
2020-10-27 13:23:49 +0200

    AlertADDinLayout

---
+++ client/src/Components/layout/alert.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Alert = ({alerts}) =>
+alerts !== 0 && 
+alerts.length > 0 &&
+alerts.map(alert => (
+    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+        {alert.msg}
+    </div>
+));
