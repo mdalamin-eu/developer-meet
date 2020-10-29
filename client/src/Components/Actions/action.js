85435e0f08a67d15db5432ba6351d877378e3431
Md Alamin <mdalamin.eu@gmail.com>
2020-10-29 16:02:05 +0200

    LoginAddOFaction

--- client/src/Components/Actions/action.js
+++ client/src/Components/Actions/action.js
@@ -26,3 +26,29 @@ dispatch({
     });
 }
 };
+
+//login
+export const loginUser = (userData, history) => async dispatch => {
+    const config = {
+        headers:{
+            "Content-Type": "application/json"
+        }
+    };
+
+    try{
+        const res = await axios.post("api/users/login", userData, config );
+        dispatch({
+            type : LOGIN_SUCCESS,
+            payload:res.data
+        });
+
+    }catch (err){
+        const errors = err.response.data.errors;
+        if(errors){
+            errors.forEach(error=> dispatch(setAlert(error.msg, "danger")));
+        }
+        dispatch({
+            type:LOGIN_FAIL
+        });
+    }
+};
