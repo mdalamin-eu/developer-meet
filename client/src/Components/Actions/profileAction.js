74aeed777c7b8d51aa11aa60a0ea9bff80cf088a
Md Alamin <mdalamin.eu@gmail.com>
2020-11-01 23:39:46 +0200

    UpdateProfileAction add currentUser

--- client/src/Components/Actions/profileAction.js
+++ client/src/Components/Actions/profileAction.js
@@ -1,2 +1,23 @@
 import axios from "axios";
-import { GET_PROFILE, GET_PROFILES, PROFILE_ERROR } from './Types'
+import { GET_PROFILE, GET_PROFILES, PROFILE_ERROR,
+                PROFILE_LOADING_START, UPDATE_PROFILE,
+                ACCOUNT_DELETE, CLEAR_ACCOUNT } from './Types'
+import {setAlert } from "./alert";
+//currentUserProfile, deleteAccount
+
+
+export const currentUserProfile = () => async dispatch=>{
+    try{
+        dispatch(profileLoadingStart());
+        const res = await axios.get ("api/profile/me");
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+    }catch(error) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload:{}
+        });
+    }
+};
