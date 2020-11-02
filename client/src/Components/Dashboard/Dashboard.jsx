3da28d1f580b0b183ae17ac0f43e2b21abb30b6a
Md Alamin <mdalamin.eu@gmail.com>
2020-11-02 23:55:38 +0200

    spinnerAdd

--- client/src/Components/Dashboard/Dashboard.jsx
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { currentUserProfile } from "../Actions/profileAction"
 
 class Dashboard extends Component {
     render() {
---
+++ client/src/Components/common/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import img from "./spinner.gif"
+
+export default function spinner () {
+    
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+
