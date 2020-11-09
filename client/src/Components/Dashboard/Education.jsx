027b73cae24b58459a158944968a485c4320e05b
Md Alamin <mdalamin.eu@gmail.com>
2020-11-09 14:57:39 +0200

    Education add in dashboard

---
+++ client/src/Components/Dashboard/Education.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Moment from "react-moment"
+import {connect } from "react-redux"
+class Education extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default  Education
