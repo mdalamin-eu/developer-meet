f9da4155d383f57564c99f73368dac4e971c9369
Md Alamin <mdalamin.eu@gmail.com>
2020-11-12 23:19:33 +0200

    addEducation in backendProfile

--- controller/Profile.js
+++ controller/Profile.js
@@ -163,3 +163,13 @@ exports.DeleteExp = async (req, res) =>{
         res.status(500).send("server error")
     }
 }
+
+
+//Education
+exports.Education= async (req, res)=> {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) { 
+        return res.status(400).json({errors: errors.array()});
+    }
+
+}
