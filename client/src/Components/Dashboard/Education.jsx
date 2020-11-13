94beabb534d91677d994eef24f901cafb6c694d4
Md Alamin <mdalamin.eu@gmail.com>
2020-11-13 21:02:04 +0200

    Education in DashboardEdu

--- client/src/Components/Dashboard/Education.jsx
+++ client/src/Components/Dashboard/Education.jsx
@@ -2,7 +2,21 @@ import React, { Component } from 'react'
 import Moment from "react-moment"
 import {connect } from "react-redux"
 class Education extends Component {
+    
+    
     render() {
+        const { education } = this.props;
+        let educationContent ;
+        if (education && education.length > 0){
+            educationContent = education.map(edu=>(
+                <tr>
+        <td>{edu.school_name}</td>
+        <td> {edu.degree} </td>
+        <td> {edu.fieldofstudy} </td>
+                </tr>
+            ))
+        } 
+        let 
         return (
             <div>
                 
