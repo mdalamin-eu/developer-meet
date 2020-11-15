98ad04a92fb66a29c6b1b628e5a1f152f255aa2f
Md Alamin <mdalamin.eu@gmail.com>
2020-11-15 20:46:15 +0200

    add CreateProfile in action

--- client/src/Components/Actions/profileAction.js
+++ client/src/Components/Actions/profileAction.js
@@ -36,12 +36,24 @@ export const createProfile = (profileData, history, isEdit= false) => async disp
        };
        dispatch(profileLoadingStart());
        const res = await axios.post("api/profile", profileData, config);
-       
+       dispatch({
+           type: GET_PROFILE,
+           payload: res.data
+       });
+       dispatch( setAlert (isEdit? "Profile Update" : "Profile Created", "success") );
+       if(!isEdit){
+           return history.push("/dashboard");
+       }
    }catch (err) {
        const errors = err.response.data.errors;
-       
+       if(errors){
+           errors.forEach(error => dispatch(setAlert(error.msg, "denger")));
+       }
+       dispatch({ type: PROFILE_ERROR,
+    payload:{msg:err.response.statusText, status:err.response.status}
+});
    }
-}
+};
 
 //delete experience by id
 
