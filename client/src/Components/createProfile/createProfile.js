9a4d32ee5836bebbd84134660add9a7e80017850
Md Alamin <mdalamin.eu@gmail.com>
2020-11-16 23:22:41 +0200

    AddSome in createProfile

--- client/src/Components/createProfile/createProfile.js
+++ client/src/Components/createProfile/createProfile.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import {WithRouter, Redirect } from "react-router-dom"
 import TextFieldGroup from "../common/TextFieldGroup"
+import { createProfile } from '../Actions/profileAction';
+import { object } from 'prop-types';
 
 class CreateProfile extends Component {
     state ={
@@ -12,8 +14,67 @@ class CreateProfile extends Component {
         skills:"",
         bio:"",
         githubusername:"",
-        
-             
+        location:"",
+        twitter:"",
+        facebook:"",
+        linkedin:"",
+        youtube:"",
+        instagram:"",
+        snapchat:"",
+        errors:{}     
+    };
+    onChange = e => {
+        this.setState({
+            [e.target.name]:e.target.value
+        });
+    };
+    onSubmit= e=> {
+        e.preventDefault();
+        const {
+            company,
+            website,
+            location,
+            bio,
+            status,
+            githubusername,
+            handle,
+            skills,
+            youtube,
+            facebook,
+            twitter,
+            instagram,
+            linkedin,
+            snapchat
+        } =this.state;
+        const CreateProfileData = {
+            company,
+            website,
+            location,
+            bio,
+            status,
+            githubusername,
+            handle,
+            skills,
+            youtube,
+            facebook,
+            twitter,
+            instagram,
+            linkedin,
+            snapchat
+        };
+        this.props.CreateProfile(createProfileData, this.props.history);
+
+    };
+    componentDidMount(){
+        this.props.currentUserProfile();
+    }
+    componentDidUpdate(prevProps) {
+        if(
+            this.props.profile.profile && 
+            object.keys(this.props.profile.profile).length > 0
+        ){
+            return this.props.history.push("/dashboard");
+        }
     }
     render() {
         return (
