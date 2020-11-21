054d8a64d8c94a2ad2a258dc08532bcd4f6a94f2
Md Alamin <mdalamin.eu@gmail.com>
2020-11-21 14:06:53 +0200

    addExperiance in profile-Form

---
+++ client/src/Components/profile-form/addExperience.js
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from 'react' //why Fragment?
+
+export default class addExperience extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
