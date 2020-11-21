5a2ad2daf1eac5df39c155cc0e5a9966c73507c8
Md Alamin <mdalamin.eu@gmail.com>
2020-11-21 22:46:50 +0200

    Update addEducation in profile-form

--- client/src/Components/profile-form/AddEducation .js
+++ client/src/Components/profile-form/AddEducation .js
@@ -6,7 +6,69 @@ import TextAreaGroup from '../common/TextAreaField'
 import {addEducation} from '../Actions/profileAction'
 
 class AddEducation extends Component {
+    state={
+        school:"",
+        degree: "",
+        fieldofstudy: "",
+        to: "",
+        from: "",
+        current: false,
+        disabled: false,
+        description: "",
+        location:""
+    };
+    onChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+    onCheck = () => {
+        this.setState({
+            current: !this.state.current,
+            disable: !this.state.disabled,
+            to: ""
+        });
+    };
+    onSubmit = e=> {
+        e.preventDefault();
+        
+        const {
+        school,
+        degree,
+        fieldofstudy,
+        to,
+        from,
+        current,
+        disabled,
+        description,
+        location
+        }= this.state
+
+    const addEducation = {
+        school,
+        degree,
+        fieldofstudy,
+        to,
+        from,
+        current,
+        disabled,
+        description,
+        location
+    }
+    this.props.addEducation(addEducation, this.props.history);
+    };
     render() {
+        const  {
+            school,
+            degree,
+            fieldofstudy,
+            to,
+            from,
+            current,
+            disabled,
+            description,
+            location
+        }= this.state;
         return (
             <div>
                 
