66d2f15a696d226804657e3c1532b33704ffdb4f
Md Alamin <mdalamin.eu@gmail.com>
2020-11-21 14:41:56 +0200

    addExperiance in profile-Form

--- client/src/Components/profile-form/addExperience.js
+++ client/src/Components/profile-form/addExperience.js
@@ -1,11 +1,77 @@
 import React, { Component, Fragment } from 'react' //why Fragment?
+import TextFieldGroup from '../common/TextFieldGroup'
+import TextAreaField from '../common/TextAreaField'
+import {connect} from 'react-redux'
+import { Link, withRouter } from 'react-router-dom';
+import {addExperience} from "../Actions/profileAction"
+class AddExperience extends Component {
+    state= {
+        company:"",
+        title:"",
+        to:"",
+        from:"",
+        current:false,
+        disabled:false,
+        location:"",
+        description:""
+    };
+    onChange = e=>{
+        this.setState({[e.targert.name]:e.target.value});
 
-export default class addExperience extends Component {
+    };
+    onCheck=e=> {
+        this.setState({
+            disabled: !this.state.disabled,
+            current: !this.state.current,
+            to: ""
+        });
+    }
+    onSubmit = e => {
+        e.preventDefault();
+    
+    const addExperience= {
+        company: this.state.company,
+        title:this.state.to,
+        to: this.state.to,
+        from: this.state.from,
+        current:this.state.current,
+        disabled:this.state.disabled,
+        location:this.state.location,
+        description:this.state.description
+    };
+    this.props.addExperience(addExperience, this.props.history);
+    };
     render() {
+        const {
+            company,
+            location,
+            title,
+            to,
+            from,
+            current,
+            disabled,
+            description
+        } =this.state
         return (
-            <div>
+            <div className="add-experience">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 m-auto">
+                            <Link to="/dashboard" className="btn btn-light">
+                                Go Back
+                            </Link>
+                            <h1 className="display-4 text-center"> Add Experience </h1>
+                            <p className="lead text-center">
+                            Add any job or position that you have had in the past or current
+                             </p>
+                             <small className="d-block pb-3">*=required fields </small>
+
+                        </div>
+                    </div>
+                </div>
                 
             </div>
         )
     }
 }
+export default  AddExperience 
