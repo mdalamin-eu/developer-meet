73b418141d7b63873d6479a7d20ae2f6533579dd
Md Alamin <mdalamin.eu@gmail.com>
2020-11-21 22:30:44 +0200

    addEducation in profile-form

---
+++ client/src/Components/profile-form/AddEducation .js
@@ -0,0 +1,17 @@
+import React, { Component} from 'react'
+import {Link, withRouter } from "react-router-dom"
+import { connect } from "react-redux"
+import TextFieldGroup from '../common/TextFieldGroup'
+import TextAreaGroup from '../common/TextAreaField'
+import {addEducation} from '../Actions/profileAction'
+
+class AddEducation extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default AddEducation 
