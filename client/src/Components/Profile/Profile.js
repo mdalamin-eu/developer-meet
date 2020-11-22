7cbf110e3f1184457d6d64f68606f47c4f7efd12
Md Alamin <mdalamin.eu@gmail.com>
2020-11-22 23:22:22 +0200

    Add  profile

---
+++ client/src/Components/Profile/Profile.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { connect } from "react-redux";
+import { getProfilefbyhandler } from "../Actions/profileAction"
+import Spinner from "../common/spinner"
+import { Link } from "react-router-dom"
+import About from "./About"
+
+
+class Profile extends Component {
+    // componentDidMount() {
+    //     if(this,props.match.params.handle) {
+    //         this
+    //     }
+    // }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default  Profile;
