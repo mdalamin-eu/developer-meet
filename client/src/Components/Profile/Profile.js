a16952294292ea284f2399671b96affc1c970881
Md Alamin <mdalamin.eu@gmail.com>
2020-11-23 22:18:17 +0200

    AddSomething

--- client/src/Components/Profile/Profile.js
+++ client/src/Components/Profile/Profile.js
@@ -7,14 +7,38 @@ import About from "./About"
 
 
 class Profile extends Component {
-    // componentDidMount() {
-    //     if(this,props.match.params.handle) {
-    //         this
-    //     }
-    // }
+    componentDidMount() {
+        if(this.props.match.params.handle) {
+            this.props.getProfilebyhandler(this.props.params.handle);
+        }
+    }
     render() {
+        const { profile, loading } = this.props.profile;
+        let profileContent;
+
+        if(profile ===null || loading ) {
+            return ( profileContent = <Spinner/>);
+        } else {
+            profileContent = (
+                <div className ="row">
+                    <div className="col-md-6">
+                        <Link className="btn btn-light md-3 float-left" to="/dashboard">
+                            Back To Home
+                        </Link>
+                    </div>
+                    < div className="col-md-6"></div>
+                </div>
+            );
+        }
         return (
-            <div className="row">
+            <div className="profile">
+                <div className="container">
+                    <div className= "row"> {profileContent}</div>
+                    {/* <Header profile={profile}/> */}
+                    <About profile={profile}/>
+                    experience={profile.experience}
+                    education={profile.education}
+                </div>
                 
             </div>
         )
