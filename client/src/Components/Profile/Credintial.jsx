60fa17853450b72a9c39bf7d8f178a5582587373
Md Alamin <mdalamin.eu@gmail.com>
2020-11-29 20:51:51 +0200

    add eduItems in Credintial

--- client/src/Components/Profile/Credintial.jsx
+++ client/src/Components/Profile/Credintial.jsx
@@ -34,7 +34,36 @@ class Credintial extends Component {
                     )}
                 </p>
             </li>
-        ))
+        ));
+        
+        const eduItems = education.map (edu => (
+            <li key={edu._id} className="list-group-item">
+                <h4>{edu.school}</h4>
+                <p>
+                    <Moment format="DD/MM/YYYY" > {edu.from} </Moment> - 
+                    {edu.to === null ? (
+                        "Now"
+                    ) : (
+                        <Moment format="DD/MM/YYYY" > {edu.to} </Moment>
+                    )}
+                </p>
+                        <p>
+                            <strong>Degree:</strong> {edu.degree}
+                        </p>
+                        <p>
+                            <strong>Field Of Study:</strong> {edu.fieldofstudy}
+                        </p>
+                        <p>
+                            {edu.description === "" ? null:(
+                                <span>
+                                    <strong> Description: </strong> {edu.description}
+                                </span>
+                            )  }
+                        </p>
+
+            </li>
+        ));
+
         return (
             <div className="row">
                 
