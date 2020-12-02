d69229b4868383b8950c9be733b3f5022478e192
Md Alamin <mdalamin.eu@gmail.com>
2020-12-02 23:52:54 +0200

    add profiles

---
+++ client/src/Components/Profiles/profiles.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class profiles extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
