8c1c4e571b8eb48187cafd967b4483ecc8bbf73e
Md Alamin <mdalamin.eu@gmail.com>
2020-12-04 19:05:27 +0200

    Update Profiles

--- client/src/Components/Profiles/profiles.jsx
+++ client/src/Components/Profiles/profiles.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
-
+import { getProfiles } from "../Actions/profileAction"
+import Spinner from "../common/spinner"
+import profileAction from "./profileItems"
 
 class Profiles extends Component {
+    componentDidMount() {
+        this.props.getProfiles();
+    }
     render() {
+        const { Profiles, loading } = this.props.profiles;
+        let profileContent;
+        if (profiles == null && loading) {
+            profileContent = <Spinner/>
+        } else {
+            if (profiles !== null && profiles.length > 0){
+                
+            }
+        }
         return (
             <div>
                 
