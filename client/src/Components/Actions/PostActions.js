e39c3744cf6f7f626b3b769233fec2d4e990dc21
Md Alamin <mdalamin.eu@gmail.com>
2020-12-09 23:13:10 +0200

    add postLike in Actions

--- client/src/Components/Actions/PostActions.js
+++ client/src/Components/Actions/PostActions.js
@@ -7,3 +7,21 @@ import { setAlert } from "./alert"
 export const startPostsLoading = () => dispatch => {
     dispatch()
 }
+
+
+
+export const postLike = id => async dispatch => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      };
+  
+      const res = await axios.put(`/api/posts/like/${id}`, config);
+      dispatch({
+        type: UPDATE_LIKES,
+        payload: { id, likes: res.data }
+      });
+    } catch (error) {}
+  };
