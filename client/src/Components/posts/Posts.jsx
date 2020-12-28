7799d97930d9b5f4f2a75a06f8c8f19a408589fb
Md Alamin <mdalamin.eu@gmail.com>
2020-12-28 23:42:36 +0200

    posts add

--- client/src/Components/posts/Posts.jsx
+++ client/src/Components/posts/Posts.jsx
@@ -12,11 +12,17 @@ class Posts extends Component {
     render(){
         const { posts, loading } = this.props.post;
         let postsContent;
+        
         if (posts === null && loading){
-            
+            postsContent = <Spinner />;
+        } else {
+            if ( posts && posts.length > 0) {
+                postsContent = <PostFeed posts={posts} />;
+            } else {
+                postsContent = <p>posts not found</p>
+            }
         }
-    }
-    render() {
+    
         return (
             <div>
                 
