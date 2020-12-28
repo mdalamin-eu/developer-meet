9406997dfe39974620be08b55616d7825cae38a1
Md Alamin <mdalamin.eu@gmail.com>
2020-12-28 23:35:38 +0200

    posts add

--- client/src/Components/posts/Posts.jsx
+++ client/src/Components/posts/Posts.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux"
 import Spinner from "../common/spinner"
+import PostFeed from "./PostFeed"
+import PostForm from "./PostForm";
 import { getposts } from "../Actions/PostActions"
 
 class Posts extends Component {
+    componentDidMount(){
+        this.props.getposts();
+    }
+    render(){
+        const { posts, loading } = this.props.post;
+        let postsContent;
+        if (posts === null && loading){
+            
+        }
+    }
     render() {
         return (
             <div>
