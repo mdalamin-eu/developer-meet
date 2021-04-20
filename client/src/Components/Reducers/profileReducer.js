57db7d76f15afe27555041d95a3a9d898202a30c
Md Alamin <mdalamin.eu@gmail.com>
2021-04-20 23:55:24 +0300

    update profile add in reducer

--- client/src/Components/Reducers/profileReducer.js
+++ client/src/Components/Reducers/profileReducer.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR, PROFILE_LOADING_START,} from '../Actions/Types';
+import { GET_PROFILE, PROFILE_ERROR, PROFILE_LOADING_START, UPDATE_PROFILE,} from '../Actions/Types';
 
 const initialState = {
     profile:null,
@@ -14,6 +14,7 @@ export default function(state= initialState, action){
                 loading: true 
             };
             case GET_PROFILE:
+            csse UPDATE_PROFILE:
                 return{
                     ...state,
                     profile:payload,
